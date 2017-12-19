76d1f70a4d25683d06f597540871f891c2693ff0
sissichen <18482257629@163.com>
2017-12-19 17:11:17 +0800

    normlise cmd

--- src/client/client1.go
+++ src/client/client1.go
@@ -7,225 +7,243 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/golang/protobuf/proto"
 
 	"chatserver/chatserver/src/protocol"
 )
 
-func main() {
-	// localaddr, err := net.ResolveTCPAddr("tcp", ":6667")
-	// serveraddr, err := net.ResolveTCPAddr("tcp", ":8080")
-	// conn, err := net.DialTCP("tcp", localaddr, serveraddr)
-	conn, err := net.Dial("tcp", ":8080")
+type ErrorCode int
 
+const (
+	CmdOK ErrorCode = iota
+	CmdTooFewArgs
+	CmdTooManyArgs
+	CmdWrongTypeArgs
+	CmdInvalid
+	CmdOffline
+)
+
+type F func(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error)
+
+var (
+	funcs = make(map[string]F)
+)
+
+func Stack() string {
+	b := bytes.NewBuffer(make([]byte, 4096))
+	runtime.Stack(b.Bytes(), false)
+	return b.String()
+}
+
+func init() {
+	funcs["login"] = login
+	funcs["chat"] = chat
+	funcs["rmcreat"] = rmcreat
+	funcs["rmjoin"] = rmjoin
+	funcs["rmchat"] = rmchat
+
+}
+func main() {
+
+	conn, err := net.Dial("tcp", ":8080")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("succeed connected to server")
 	defer conn.Close()
-	go readfromServer(conn)
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err.(error))
+			fmt.Println(Stack())
+		}
+	}()
+	offline := true
+	go readfromServer(conn, &offline)
 	reader := bufio.NewReader(os.Stdin)
-
 	for {
-		//read msgtype
-		fmt.Println("please input msg type")
-		msgtype, _, err := reader.ReadLine()
+		msg, _, err := reader.ReadLine()
 		if err != nil {
-			fmt.Println("read msgtype wrong!")
+			panic(err)
+		}
+		msgstr := string(msg)
+		line := strings.SplitN(msgstr, " ", 2)
+		if len(line) != 2 {
+			fmt.Println("too few args!")
+			continue
 		}
 
-		str := string(msgtype)
-		fmt.Println(str)
-		var msgType protocol.MsgType
-		switch str {
-		case "LI":
-			fmt.Println("please inout userid")
-			msgType = protocol.MsgType_MT_LOGIN_REQUEST
-			loginReq := &protocol.LoginRequest{}
-			userid, _, err := reader.ReadLine()
-			if err != nil {
-				fmt.Println("read msgtype wrong!")
-			}
-
-			userIDint, _ := strconv.Atoi(string(userid))
-			loginReq.UserID = int64(userIDint)
-			fmt.Printf("userid is %d\n", loginReq.UserID)
-
-			data, err := proto.Marshal(loginReq)
-			fmt.Println(string(data))
-			if err != nil {
-				fmt.Println("wrong user id")
-				continue
-			}
-
-			size := len(data)
-			fmt.Println(size)
-			out := bytes.NewBuffer(make([]byte, 0, 4+size))
-			if err := binary.Write(out, binary.LittleEndian, uint16(msgType)); err != nil {
-				fmt.Println("failed to write msgtype to buffer")
-			}
-			if err := binary.Write(out, binary.LittleEndian, uint16(size)); err != nil {
-				fmt.Println("failed to write msgsize to buffer")
-			}
-			if err := binary.Write(out, binary.LittleEndian, data); err != nil {
-				fmt.Println("failed to write msgbody to buffer")
-			}
-			//fmt.Println(out.Bytes())
-			n, err := conn.Write(out.Bytes())
-			fmt.Printf("sent %d bytes to server\n", n)
-			if err != nil {
-				fmt.Println("failed to write to server")
-				return
-			}
-		case "PC":
-			fmt.Println("please input target userid")
-			msgType = protocol.MsgType_MT_PRIVCHAT_REQUEST
-			privChatReq := &protocol.PrivateChatRequest{}
-			userid, _, err := reader.ReadLine()
-			if err != nil {
-				fmt.Println("read msgtype wrong!")
-			}
-			userIDint, _ := strconv.Atoi(string(userid))
-			privChatReq.Target = int64(userIDint)
-			fmt.Printf("userid is %d\n", privChatReq.Target)
-
-			privChatReq.Content, err = reader.ReadString('\n')
-			fmt.Println(privChatReq.Content)
-
-			data, err := proto.Marshal(privChatReq)
-			if err != nil {
-				fmt.Println("wrong user id")
-				continue
-			}
-
-			size := len(data)
-			out := bytes.NewBuffer(make([]byte, 0, 4+size))
-			if err := binary.Write(out, binary.LittleEndian, uint16(msgType)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, uint16(size)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, data); err != nil {
-				// TODO
-			}
-
-			n, err := conn.Write(out.Bytes())
-			fmt.Printf("sent %d bytes to server\n", n)
-			if err != nil {
-				fmt.Println("failed to write to server")
-				return
-			}
-		case "RC":
-			fmt.Println("please input new room name")
-			msgType = protocol.MsgType_MT_RMCREAT_REQUEST
-			rmCreatReq := &protocol.RoomCreateRequest{}
-			rmCreatReq.Name, err = reader.ReadString('\n')
-			fmt.Println(rmCreatReq.Name)
-			data, err := proto.Marshal(rmCreatReq)
-			if err != nil {
-				fmt.Println("wrong user id")
-				continue
-			}
-			size := len(data)
-			out := bytes.NewBuffer(make([]byte, 0, 4+size))
-			if err := binary.Write(out, binary.LittleEndian, uint16(msgType)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, uint16(size)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, data); err != nil {
-				// TODO
-			}
-
-			_, err = conn.Write(out.Bytes())
-			if err != nil {
-				fmt.Println("failed to write to server")
-				return
-			}
-		case "RJ":
-			fmt.Println("please input room id")
-			msgType = protocol.MsgType_MT_RMJOIN_REQUEST
-			rmJoinReq := &protocol.RoomJoinRequest{}
-			userid, _, err := reader.ReadLine()
-			if err != nil {
-				fmt.Println("read msgtype wrong!")
-			}
-			userIDint, _ := strconv.Atoi(string(userid))
-			rmJoinReq.RoomID = int64(userIDint)
-			fmt.Printf("userid is %d\n", rmJoinReq.RoomID)
-
-			data, err := proto.Marshal(rmJoinReq)
-			if err != nil {
-				fmt.Println("wrong user id")
-				continue
-			}
-			size := len(data)
-			out := bytes.NewBuffer(make([]byte, 0, 4+size))
-			if err := binary.Write(out, binary.LittleEndian, uint16(msgType)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, uint16(size)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, data); err != nil {
-				// TODO
-			}
-
-			_, err = conn.Write(out.Bytes())
-			if err != nil {
-				fmt.Println("failed to write to server")
-				return
-			}
-		case "GC":
-			fmt.Println("please input roomid")
-			msgType = protocol.MsgType_MT_RMCHAT_REQUSET
-			rmChatReq := &protocol.RoomChatRequest{}
-			userid, _, err := reader.ReadLine()
-			if err != nil {
-				fmt.Println("read msgtype wrong!")
-			}
-			userIDint, _ := strconv.Atoi(string(userid))
-			rmChatReq.RoomID = int64(userIDint)
-			fmt.Printf("userid is %d\n", rmChatReq.RoomID)
-
-			fmt.Println("please input msg content")
-			rmChatReq.Content, err = reader.ReadString('\n')
-			fmt.Println(rmChatReq.Content)
-
-			data, err := proto.Marshal(rmChatReq)
-			if err != nil {
-				fmt.Println("wrong user id")
-				continue
-			}
-
-			size := len(data)
-			out := bytes.NewBuffer(make([]byte, 0, 4+size))
-			if err := binary.Write(out, binary.LittleEndian, uint16(msgType)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, uint16(size)); err != nil {
-				// TODO
-			}
-			if err := binary.Write(out, binary.LittleEndian, data); err != nil {
-				// TODO
-			}
-
-			_, err = conn.Write(out.Bytes())
-			if err != nil {
-				fmt.Println("failed to write to server")
-				return
-			}
-		default:
-			fmt.Println("wrong type ! ")
+		f, ok := funcs[line[0]]
+		if !ok {
+			fmt.Println("invalid cmd!")
+			continue
 		}
 
+		msgtype, data, ec, err := f(line[1], &offline)
+		if err != nil {
+			panic(err)
+		}
+		if ec != CmdOK {
+			handleEC(ec)
+			continue
+		}
+
+		tosend, err := writeBuffer(msgtype, data)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = conn.Write(tosend)
+		if err != nil {
+			fmt.Println("failed to write to server")
+			return
+		}
 	}
 }
-func readfromServer(conn net.Conn) {
+
+func login(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
+	ec := CmdOK
+	var err error
+	var data []byte
+	msgType := protocol.MsgType_MT_LOGIN_REQUEST
+	args := strings.Split(line, " ")
+	if len(args) == 1 {
+		loginReq := &protocol.LoginRequest{}
+		nUserID, err := strconv.Atoi(args[0])
+		if err != nil {
+			return msgType, nil, CmdWrongTypeArgs, err
+		}
+		loginReq.UserID = int64(nUserID)
+		data, err = proto.Marshal(loginReq)
+	} else {
+		ec = CmdTooManyArgs
+	}
+	return msgType, data, ec, err
+}
+
+func chat(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
+	ec := CmdOK
+	var err error
+	var data []byte
+	msgType := protocol.MsgType_MT_PRIVCHAT_REQUEST
+	if *offline {
+		return msgType, nil, CmdOffline, err
+	}
+	args := strings.SplitN(line, " ", 2)
+	if len(args) == 2 {
+		privChatReq := &protocol.PrivateChatRequest{}
+		nUserID, err := strconv.Atoi(args[0])
+		if err != nil {
+			return msgType, nil, CmdWrongTypeArgs, err
+		}
+		privChatReq.Target = int64(nUserID)
+		privChatReq.Content = args[1]
+		data, err = proto.Marshal(privChatReq)
+	} else {
+		ec = CmdTooManyArgs
+	}
+	return msgType, data, ec, err
+}
+
+func rmcreat(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
+	ec := CmdOK
+	var err error
+	var data []byte
+	msgType := protocol.MsgType_MT_RMCREAT_REQUEST
+	if *offline {
+		return msgType, nil, CmdOffline, err
+	}
+	args := strings.Split(line, " ")
+	if len(args) == 1 {
+		rmCreatReq := &protocol.RoomCreateRequest{}
+		rmCreatReq.Name = args[0]
+		data, err = proto.Marshal(rmCreatReq)
+	} else {
+		ec = CmdTooManyArgs
+	}
+	return msgType, data, ec, err
+}
+func rmjoin(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
+	ec := CmdOK
+	var err error
+	var data []byte
+	msgType := protocol.MsgType_MT_RMJOIN_REQUEST
+	if *offline {
+		return msgType, nil, CmdOffline, err
+	}
+	args := strings.Split(line, " ")
+	if len(args) == 1 {
+		rmJoinReq := &protocol.RoomJoinRequest{}
+		nRoomID, err := strconv.Atoi(args[0])
+		if err != nil {
+			return msgType, nil, CmdWrongTypeArgs, err
+		}
+		rmJoinReq.RoomID = int64(nRoomID)
+		data, err = proto.Marshal(rmJoinReq)
+	} else {
+		ec = CmdTooManyArgs
+	}
+	return msgType, data, ec, err
+}
+func rmchat(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
+	ec := CmdOK
+	var err error
+	var data []byte
+	msgType := protocol.MsgType_MT_RMCHAT_REQUSET
+	if *offline {
+		return msgType, nil, CmdOffline, err
+	}
+	args := strings.SplitN(line, " ", 2)
+	if len(args) == 2 {
+		rmChatReq := &protocol.RoomChatRequest{}
+		nRoomID, err := strconv.Atoi(args[0])
+		if err != nil {
+			return msgType, nil, CmdWrongTypeArgs, err
+		}
+		rmChatReq.RoomID = int64(nRoomID)
+		rmChatReq.Content = args[1]
+		data, err = proto.Marshal(rmChatReq)
+	} else {
+		ec = CmdTooFewArgs
+	}
+	return msgType, data, ec, err
+}
+
+func handleEC(ec ErrorCode) {
+	switch ec {
+	case CmdTooFewArgs:
+		fmt.Println("two few args!")
+	case CmdTooManyArgs:
+		fmt.Println("two many args!")
+	case CmdWrongTypeArgs:
+		fmt.Println("wrong args type!")
+	case CmdInvalid:
+		fmt.Println("invalid cmd!!")
+	case CmdOffline:
+		fmt.Println("login first!!")
+	}
+}
+
+func writeBuffer(msgtype protocol.MsgType, msg []byte) ([]byte, error) {
+	var err error
+	size := len(msg)
+	buf := bytes.NewBuffer(make([]byte, 0, 4+size))
+	if err = binary.Write(buf, binary.LittleEndian, uint16(msgtype)); err != nil {
+		panic(err)
+	}
+	if err = binary.Write(buf, binary.LittleEndian, uint16(size)); err != nil {
+		panic(err)
+	}
+	if err = binary.Write(buf, binary.LittleEndian, msg); err != nil {
+		panic(err)
+	}
+	return buf.Bytes(), err
+}
+
+func readfromServer(conn net.Conn, offline *bool) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	for {
@@ -243,7 +261,6 @@ func readfromServer(conn net.Conn) {
 			return
 		}
 		msgtype = protocol.MsgType(msgtypeint16)
-		fmt.Printf("received msgtype %d\n", msgtype)
 		//read message size
 		head := make([]byte, 2)
 		if err := binary.Read(reader, binary.LittleEndian, head); err != nil {
@@ -258,7 +275,7 @@ func readfromServer(conn net.Conn) {
 			return
 		}
 		size = int(sizeint16)
-		fmt.Printf("received msg size %d\n", size)
+		//fmt.Printf("received msg size %d\n", size)
 		//read message
 		msgbody := make([]byte, size)
 		if err := binary.Read(reader, binary.LittleEndian, msgbody); err != nil {
@@ -271,7 +288,12 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, loginResp); err != nil {
 				fmt.Println("unmarshal response failed!")
 			} else {
-				fmt.Printf("log in response with code:%d\n", loginResp.Ec)
+				if loginResp.Ec == protocol.ErrorCode_EC_OK {
+					fmt.Println("Log in OK")
+					*offline = false
+				} else {
+					fmt.Println("Log in Failed")
+				}
 			}
 
 		case protocol.MsgType_MT_PRIVCHAT_RESPONSE:
@@ -279,7 +301,11 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, privChatResp); err != nil {
 				fmt.Println("unmarshal response failed!")
 			} else {
-				fmt.Printf("privatechat response with code:%d\n", privChatResp.Ec)
+				if privChatResp.Ec == protocol.ErrorCode_EC_OK {
+					fmt.Println("private chat OK")
+				} else {
+					fmt.Println("Target Not Found")
+				}
 			}
 
 		case protocol.MsgType_MT_RMCREAT_RESPOSE:
@@ -287,15 +313,22 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, rmCreatResp); err != nil {
 				fmt.Println("unmarshal response failed!")
 			} else {
-				fmt.Printf("roomcreat response with code:%d\n", rmCreatResp.Ec)
-				fmt.Printf("creatded room id :%d\n", rmCreatResp.RoomID)
+				if rmCreatResp.Ec == protocol.ErrorCode_EC_OK {
+					fmt.Printf("creatded room %d OK \n", rmCreatResp.RoomID)
+				} else {
+					fmt.Printf("room create Fail with code:%d\n", rmCreatResp.Ec)
+				}
 			}
 		case protocol.MsgType_MT_RMJOIN_RESPONSE:
 			rmJoinResp := &protocol.RoomJoinResponse{}
 			if err := proto.Unmarshal(msgbody, rmJoinResp); err != nil {
 				fmt.Println("unmarshal response failed!")
 			} else {
-				fmt.Printf("roomjoin response with code:%d\n", rmJoinResp.Ec)
+				if rmJoinResp.Ec == protocol.ErrorCode_EC_OK {
+					fmt.Println("room join OK")
+				} else {
+					fmt.Println("room join Failed")
+				}
 			}
 
 		case protocol.MsgType_MT_RMCHAT_RESPONSE:
@@ -303,7 +336,11 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, rmChatResp); err != nil {
 				fmt.Println("unmarshal response failed!")
 			} else {
-				fmt.Printf("roomchat response with code:%d\n", rmChatResp.Ec)
+				if rmChatResp.Ec == protocol.ErrorCode_EC_OK {
+					fmt.Println("room chat OK")
+				} else {
+					fmt.Println("room chat Failed")
+				}
 			}
 
 		case protocol.MsgType_MT_PRIVCHAT_NOYIFY:
@@ -311,8 +348,7 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, privChatNotify); err != nil {
 				fmt.Println("unmarshal notify failed!")
 			} else {
-				fmt.Printf("receive from [%d] ", privChatNotify.Src)
-				fmt.Println(privChatNotify.Content)
+				fmt.Printf(privChatNotify.Content+" from [%d]\n", privChatNotify.Src)
 			}
 
 		case protocol.MsgType_MT_RMCHAT_NOTIFY:
@@ -320,9 +356,15 @@ func readfromServer(conn net.Conn) {
 			if err := proto.Unmarshal(msgbody, roomChatNotify); err != nil {
 				fmt.Println("unmarshal notify failed!")
 			} else {
-				fmt.Printf("receive from [%d] ", roomChatNotify.UserID)
-				fmt.Printf("in group[%d]:", roomChatNotify.RoomID)
-				fmt.Println(roomChatNotify.Content)
+				fmt.Printf(roomChatNotify.Content+" from [%d] in room [%d]\n", roomChatNotify.UserID, roomChatNotify.RoomID)
+			}
+		case protocol.MsgType_MT_OFFLINE_NOYIFY:
+			offlineNotify := &protocol.OfflineNotify{}
+			if err := proto.Unmarshal(msgbody, offlineNotify); err != nil {
+				fmt.Println("unmarshal notify failed!")
+			} else {
+				fmt.Printf("[%d] offline\n", offlineNotify.UserID)
+				*offline = true
 			}
 		default:
 			//type error
