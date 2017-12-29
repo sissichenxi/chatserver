4823930f6f3137bb91a8187135881bbd79395681
sissichen <18482257629@163.com>
2017-12-29 11:44:11 +0800

    define room related informations and roomchat msg

---
+++ src/server/roomchat.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"chatserver/chatserver/src/protocol"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+)
+
+type roomInfo struct {
+	Roomid   int64   `bson:"roomid"`
+	Roomname string  `bson:"roomname"`
+	Memid    []int64 `bson:"memid"`
+}
+
+type roomMsg struct {
+	Roomid  int64     `bson:"roomid"`
+	Srcid   int64     `bson:"srcid"`
+	Content string    `bson:"content"`
+	Time    time.Time `bson:"time"`
+}
+
+type IDgetter struct {
+	roomid int64
+	mu     sync.Mutex
+}
+
+func (idget *IDgetter) spanNextID() (nextid int64) {
+	idget.mu.Lock()
+	idget.roomid++
+	nextid = idget.roomid
+	idget.mu.Unlock()
+	return
+}
+
+const maxRoomMember = 100
+
+var chatRooms []roomInfo
+var roommap map[int64]*roomInfo
+
+func (rmmsg *roomMsg) ResolveMsg() []byte {
+	rmchatNotify := &protocol.RoomChatNotify{}
+	rmchatNotify.UserID = rmmsg.Srcid
+	rmchatNotify.RoomID = rmmsg.Roomid
+	rmchatNotify.Content = rmmsg.Content
+	notifytype := protocol.MsgType_MT_RMCHAT_NOTIFY
+	notifymsg, err := proto.Marshal(rmchatNotify)
+	if err != nil {
+		fmt.Println("marshal rmchatNotify faliled!")
+	}
+	notify, err := writeBuffer(notifytype, notifymsg)
+	if err != nil {
+		panic(err)
+	}
+	return notify.Bytes()
+}
