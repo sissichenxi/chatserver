4bab727a2fab06af55aaa47ff6f3b974a9a36e58
sissichen <18482257629@163.com>
2017-12-29 11:42:50 +0800

    adjust funcions

--- src/client/client1.go
+++ src/client/client1.go
@@ -27,10 +27,12 @@ const (
 	CmdOffline
 )
 
-type F func(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error)
+type PCmdFunc func(string, *bool) (protocol.MsgType, []byte, ErrorCode, error)
+type PMsgFunc func([]byte, *bool)
 
 var (
-	funcs = make(map[string]F)
+	cmdfuncs = make(map[string]PCmdFunc)
+	msgfuncs = make(map[protocol.MsgType]PMsgFunc)
 )
 
 func Stack() string {
@@ -40,12 +42,19 @@ func Stack() string {
 }
 
 func init() {
-	funcs["login"] = login
-	funcs["chat"] = chat
-	funcs["rmcreat"] = rmcreat
-	funcs["rmjoin"] = rmjoin
-	funcs["rmchat"] = rmchat
-
+	cmdfuncs["login"] = login
+	cmdfuncs["chat"] = chat
+	cmdfuncs["rmcreat"] = rmcreat
+	cmdfuncs["rmjoin"] = rmjoin
+	cmdfuncs["rmchat"] = rmchat
+	msgfuncs[protocol.MsgType_MT_LOGIN_RESPONSE] = loginParse
+	msgfuncs[protocol.MsgType_MT_PRIVCHAT_RESPONSE] = chatRespParse
+	msgfuncs[protocol.MsgType_MT_RMCREAT_RESPOSE] = rmCreatParse
+	msgfuncs[protocol.MsgType_MT_RMJOIN_RESPONSE] = rmJoinParse
+	msgfuncs[protocol.MsgType_MT_RMCHAT_RESPONSE] = rmChatRespParse
+	msgfuncs[protocol.MsgType_MT_PRIVCHAT_NOYIFY] = chatNotifyParse
+	msgfuncs[protocol.MsgType_MT_RMCHAT_NOTIFY] = rmChatNotifyParse
+	msgfuncs[protocol.MsgType_MT_OFFLINE_NOYIFY] = offlineNotifyParse
 }
 func main() {
 
@@ -76,7 +85,7 @@ func main() {
 			continue
 		}
 
-		f, ok := funcs[line[0]]
+		f, ok := cmdfuncs[line[0]]
 		if !ok {
 			fmt.Println("invalid cmd!")
 			continue
@@ -102,6 +111,79 @@ func main() {
 			return
 		}
 	}
+}
+
+func writeBuffer(msgtype protocol.MsgType, msg []byte) ([]byte, error) {
+	var err error
+	size := len(msg)
+	buf := bytes.NewBuffer(make([]byte, 0, 4+size))
+	if err = binary.Write(buf, binary.LittleEndian, uint16(msgtype)); err != nil {
+		panic(err)
+	}
+	if err = binary.Write(buf, binary.LittleEndian, uint16(size)); err != nil {
+		panic(err)
+	}
+	if err = binary.Write(buf, binary.LittleEndian, msg); err != nil {
+		panic(err)
+	}
+	return buf.Bytes(), err
+}
+
+func readfromServer(conn net.Conn, offline *bool) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		msgtype, msgbody, err := readParse(reader)
+		if err != nil {
+			return
+		}
+		f, ok := msgfuncs[msgtype]
+		if !ok {
+			fmt.Println("undefined msg type!")
+			continue
+		}
+		f(msgbody, offline)
+	}
+}
+func readParse(reader *bufio.Reader) (protocol.MsgType, []byte, error) {
+	var msgtype protocol.MsgType
+	//read message size
+	head := make([]byte, 2)
+	if err := binary.Read(reader, binary.LittleEndian, head); err != nil {
+		fmt.Println("read msg len wrong!")
+		return msgtype, nil, err
+	}
+	var size int
+	var sizeint16 int16
+	buf := bytes.NewBuffer(head)
+	if err := binary.Read(buf, binary.LittleEndian, &sizeint16); err != nil {
+		fmt.Println("read msg body wrong!")
+		return msgtype, nil, err
+	}
+	size = int(sizeint16)
+
+	mtype := make([]byte, 2)
+	if err := binary.Read(reader, binary.LittleEndian, mtype); err != nil {
+		fmt.Println("read msg type wrong!")
+		return msgtype, nil, err
+	}
+
+	var msgtypeint16 int16
+	typebuf := bytes.NewBuffer(mtype)
+	if err := binary.Read(typebuf, binary.LittleEndian, &msgtypeint16); err != nil {
+		fmt.Println("read msgtype wrong!")
+		return msgtype, nil, err
+	}
+	msgtype = protocol.MsgType(msgtypeint16)
+
+	//fmt.Printf("received msg size %d\n", size)
+	//read message
+	msgbody := make([]byte, size)
+	if err := binary.Read(reader, binary.LittleEndian, msgbody); err != nil {
+		fmt.Println("read msg body byte wrong!")
+		return msgtype, nil, err
+	}
+	return msgtype, msgbody, nil
 }
 
 func login(line string, offline *bool) (protocol.MsgType, []byte, ErrorCode, error) {
@@ -226,149 +308,89 @@ func handleEC(ec ErrorCode) {
 		fmt.Println("login first!!")
 	}
 }
-
-func writeBuffer(msgtype protocol.MsgType, msg []byte) ([]byte, error) {
-	var err error
-	size := len(msg)
-	buf := bytes.NewBuffer(make([]byte, 0, 4+size))
-	if err = binary.Write(buf, binary.LittleEndian, uint16(msgtype)); err != nil {
-		panic(err)
+func loginParse(msgbody []byte, offline *bool) {
+	loginResp := &protocol.LoginResponse{}
+	if err := proto.Unmarshal(msgbody, loginResp); err != nil {
+		fmt.Println("unmarshal response failed!")
+	} else {
+		if loginResp.Ec == protocol.ErrorCode_EC_OK {
+			fmt.Println("Log in OK")
+			*offline = false
+		} else {
+			fmt.Println("Log in Failed")
+		}
 	}
-	if err = binary.Write(buf, binary.LittleEndian, uint16(size)); err != nil {
-		panic(err)
-	}
-	if err = binary.Write(buf, binary.LittleEndian, msg); err != nil {
-		panic(err)
-	}
-	return buf.Bytes(), err
 }
-
-func readfromServer(conn net.Conn, offline *bool) {
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-	for {
-		//read message type
-		mtype := make([]byte, 2)
-		if err := binary.Read(reader, binary.LittleEndian, mtype); err != nil {
-			fmt.Println("read msg type wrong!")
-			return
+func chatRespParse(msgbody []byte, offline *bool) {
+	privChatResp := &protocol.PrivateChatResponse{}
+	if err := proto.Unmarshal(msgbody, privChatResp); err != nil {
+		fmt.Println("unmarshal response failed!")
+	} else {
+		if privChatResp.Ec == protocol.ErrorCode_EC_OK {
+			fmt.Println("private chat OK")
+		} else {
+			fmt.Println("Target Not Found")
 		}
-		var msgtype protocol.MsgType
-		var msgtypeint16 int16
-		typebuf := bytes.NewBuffer(mtype)
-		if err := binary.Read(typebuf, binary.LittleEndian, &msgtypeint16); err != nil {
-			fmt.Println("read msgtype wrong!")
-			return
+	}
+}
+func rmCreatParse(msgbody []byte, offline *bool) {
+	rmCreatResp := &protocol.RoomCreateResponse{}
+	if err := proto.Unmarshal(msgbody, rmCreatResp); err != nil {
+		fmt.Println("unmarshal response failed!")
+	} else {
+		if rmCreatResp.Ec == protocol.ErrorCode_EC_OK {
+			fmt.Printf("creatded room %d OK \n", rmCreatResp.RoomID)
+		} else {
+			fmt.Printf("room create Fail with code:%d\n", rmCreatResp.Ec)
 		}
-		msgtype = protocol.MsgType(msgtypeint16)
-		//read message size
-		head := make([]byte, 2)
-		if err := binary.Read(reader, binary.LittleEndian, head); err != nil {
-			fmt.Println("read msg len wrong!")
-			return
+	}
+}
+func rmJoinParse(msgbody []byte, offline *bool) {
+	rmJoinResp := &protocol.RoomJoinResponse{}
+	if err := proto.Unmarshal(msgbody, rmJoinResp); err != nil {
+		fmt.Println("unmarshal response failed!")
+	} else {
+		if rmJoinResp.Ec == protocol.ErrorCode_EC_OK {
+			fmt.Println("room join OK")
+		} else {
+			fmt.Println("room join Failed")
 		}
-		var size int
-		var sizeint16 int16
-		buf := bytes.NewBuffer(head)
-		if err := binary.Read(buf, binary.LittleEndian, &sizeint16); err != nil {
-			fmt.Println("read msg body wrong!")
-			return
+	}
+}
+func rmChatRespParse(msgbody []byte, offline *bool) {
+	rmChatResp := &protocol.RoomChatResponse{}
+	if err := proto.Unmarshal(msgbody, rmChatResp); err != nil {
+		fmt.Println("unmarshal response failed!")
+	} else {
+		if rmChatResp.Ec == protocol.ErrorCode_EC_OK {
+			fmt.Println("room chat OK")
+		} else {
+			fmt.Println("room chat Failed")
 		}
-		size = int(sizeint16)
-		//fmt.Printf("received msg size %d\n", size)
-		//read message
-		msgbody := make([]byte, size)
-		if err := binary.Read(reader, binary.LittleEndian, msgbody); err != nil {
-			fmt.Println("read msg body byte wrong!")
-			return
-		}
-		switch msgtype {
-		case protocol.MsgType_MT_LOGIN_RESPONSE:
-			loginResp := &protocol.LoginResponse{}
-			if err := proto.Unmarshal(msgbody, loginResp); err != nil {
-				fmt.Println("unmarshal response failed!")
-			} else {
-				if loginResp.Ec == protocol.ErrorCode_EC_OK {
-					fmt.Println("Log in OK")
-					*offline = false
-				} else {
-					fmt.Println("Log in Failed")
-				}
-			}
-
-		case protocol.MsgType_MT_PRIVCHAT_RESPONSE:
-			privChatResp := &protocol.PrivateChatResponse{}
-			if err := proto.Unmarshal(msgbody, privChatResp); err != nil {
-				fmt.Println("unmarshal response failed!")
-			} else {
-				if privChatResp.Ec == protocol.ErrorCode_EC_OK {
-					fmt.Println("private chat OK")
-				} else {
-					fmt.Println("Target Not Found")
-				}
-			}
-
-		case protocol.MsgType_MT_RMCREAT_RESPOSE:
-			rmCreatResp := &protocol.RoomCreateResponse{}
-			if err := proto.Unmarshal(msgbody, rmCreatResp); err != nil {
-				fmt.Println("unmarshal response failed!")
-			} else {
-				if rmCreatResp.Ec == protocol.ErrorCode_EC_OK {
-					fmt.Printf("creatded room %d OK \n", rmCreatResp.RoomID)
-				} else {
-					fmt.Printf("room create Fail with code:%d\n", rmCreatResp.Ec)
-				}
-			}
-		case protocol.MsgType_MT_RMJOIN_RESPONSE:
-			rmJoinResp := &protocol.RoomJoinResponse{}
-			if err := proto.Unmarshal(msgbody, rmJoinResp); err != nil {
-				fmt.Println("unmarshal response failed!")
-			} else {
-				if rmJoinResp.Ec == protocol.ErrorCode_EC_OK {
-					fmt.Println("room join OK")
-				} else {
-					fmt.Println("room join Failed")
-				}
-			}
-
-		case protocol.MsgType_MT_RMCHAT_RESPONSE:
-			rmChatResp := &protocol.RoomChatResponse{}
-			if err := proto.Unmarshal(msgbody, rmChatResp); err != nil {
-				fmt.Println("unmarshal response failed!")
-			} else {
-				if rmChatResp.Ec == protocol.ErrorCode_EC_OK {
-					fmt.Println("room chat OK")
-				} else {
-					fmt.Println("room chat Failed")
-				}
-			}
-
-		case protocol.MsgType_MT_PRIVCHAT_NOYIFY:
-			privChatNotify := &protocol.PrivateChatNotify{}
-			if err := proto.Unmarshal(msgbody, privChatNotify); err != nil {
-				fmt.Println("unmarshal notify failed!")
-			} else {
-				fmt.Printf(privChatNotify.Content+" from [%d]\n", privChatNotify.Src)
-			}
-
-		case protocol.MsgType_MT_RMCHAT_NOTIFY:
-			roomChatNotify := &protocol.RoomChatNotify{}
-			if err := proto.Unmarshal(msgbody, roomChatNotify); err != nil {
-				fmt.Println("unmarshal notify failed!")
-			} else {
-				fmt.Printf(roomChatNotify.Content+" from [%d] in room [%d]\n", roomChatNotify.UserID, roomChatNotify.RoomID)
-			}
-		case protocol.MsgType_MT_OFFLINE_NOYIFY:
-			offlineNotify := &protocol.OfflineNotify{}
-			if err := proto.Unmarshal(msgbody, offlineNotify); err != nil {
-				fmt.Println("unmarshal notify failed!")
-			} else {
-				fmt.Printf("[%d] offline\n", offlineNotify.UserID)
-				*offline = true
-			}
-		default:
-			//type error
-			fmt.Println("wrong msg type!")
-		}
+	}
+}
+func chatNotifyParse(msgbody []byte, offline *bool) {
+	privChatNotify := &protocol.PrivateChatNotify{}
+	if err := proto.Unmarshal(msgbody, privChatNotify); err != nil {
+		fmt.Println("unmarshal notify failed!")
+	} else {
+		fmt.Printf(privChatNotify.Content+" from [%d]\n", privChatNotify.Src)
+	}
+}
+func rmChatNotifyParse(msgbody []byte, offline *bool) {
+	roomChatNotify := &protocol.RoomChatNotify{}
+	if err := proto.Unmarshal(msgbody, roomChatNotify); err != nil {
+		fmt.Println("unmarshal notify failed!")
+	} else {
+		fmt.Printf(roomChatNotify.Content+" from [%d] in room [%d]\n", roomChatNotify.UserID, roomChatNotify.RoomID)
+	}
+}
+func offlineNotifyParse(msgbody []byte, offline *bool) {
+	offlineNotify := &protocol.OfflineNotify{}
+	if err := proto.Unmarshal(msgbody, offlineNotify); err != nil {
+		fmt.Println("unmarshal notify failed!")
+	} else {
+		fmt.Printf("[%d] offline\n", offlineNotify.UserID)
+		*offline = true
 	}
 }
